61c30760666f725e0705405d7978285367f71724
Radosław Siek <Borderlandsmaniak@gmail.com>
2023-09-26 22:09:54 +0200

    Data Today

--- src/components/StatsToday.jsx
+++ src/components/StatsToday.jsx
@@ -3,8 +3,8 @@ import SingleToday from "./SingleToday";
 
 const StatsToday = ({ todayStats }) => {
   return (
-    <div className="container mx-auto pb-8">
-      <h2 className="mb-6 text-2xl font-bold text-neutral-darkTheme-desaturatedBlue dark:text-neutral-lightTheme-veryPaleBlue">
+    <div className="container mx-auto pb-20">
+      <h2 className="mb-6 pl-5 text-2xl font-bold text-neutral-darkTheme-desaturatedBlue dark:text-neutral-lightTheme-veryPaleBlue md:pl-0">
         Overview - Today
       </h2>
       <div className="stats-wrapper">
